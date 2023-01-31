a2b5f5bcbd8f98bca59d5ba2afe682a764b1a8a7
AngelinaShoiliRozario <shoili.rozario@gmail.com>
2023-01-31 16:55:18 +0600

    first commit

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -9,7 +9,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     
-    
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     
     <style>
         body{
@@ -73,9 +73,12 @@
               </li>
             <li class="nav-item dropdown">
               <a class="nav-link " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="{{asset('/assets/images/ssa.png')}}" alt="" style="width:20px; height:20px;">
+                {{-- <img src="{{asset('/assets/images/ssa.png')}}" alt="" style="width:20px; height:20px;"> --}}
+                <span class="material-symbols-outlined">
+                    more_horiz
+                    </span>
               </a>
-              <div class="dropdown-menu " style="padding: 9px 12px;">
+              <div class="dropdown-menu " style="padding: 9px 6px;">
                 <input type="text" class="form-control" id="myInput"/>
                 <ul  style="width: 240px;list-style-type:none;padding:0;" id="myList">
                    
@@ -96,7 +99,7 @@
                     <li><a class="dropdown-item" href="#">Visits</a></li>
                     <li><a class="dropdown-item" href="#">Social</a></li>
                     <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    <li><a class="dropdown-item" href="#">Create New Module</a></li>
                   </ul>
               </div>
               
